ade6a156f73565223d7573ed69faab4d55ab871c
Thiago-Luiz-casagrande <thiagocasagrande2004@gmail.com>
2025-02-25 23:19:48 -0300

    commit-20

--- src/Calculadora/Calculadora.jsx
+++ src/Calculadora/Calculadora.jsx
@@ -34,7 +34,7 @@ function Calculadora() {
     return () => window.removeEventListener("resize", handleResize);
   }, [isMobile]);
 
-  // forms
+  // fazer forms
 
   const [branco, setBranco] = useState("Branco");
   const [solido, setSolido] = useState("Sólido");
